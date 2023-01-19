4872f4ae45e0860f2889d937bb15d40f302c8048
Giovanna Niemeyer <giovanna.niemeyer@gmail.com>
2023-01-18 23:52:39 -0300

    fix: remove unecessary comments

--- algorithms/multiple-pointers.js
+++ algorithms/multiple-pointers.js
@@ -52,8 +52,6 @@ function countUniqueValues(arr) {
 }
 
 countUniqueValues([ 1 , 1 , 1 , 1 , 1 , 2]) // 2
-                // p1   p2              p1
 countUniqueValues([1 , 2 , 3 , 4 , 4 , 4 , 7 , 7 , 12 , 12 , 13]) // 7
-
 countUniqueValues([]) // 0
 countUniqueValues([-2,-1,-1,0,1]) // 4
